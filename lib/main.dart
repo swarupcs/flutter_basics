5f2da3b569fb97fd61663039237cab919495e35e
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-07-06 11:15:45 +0530

    Center Widget 1

--- lib/main.dart
+++ lib/main.dart
@@ -42,12 +42,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text("Flutter Container"),
       ),
       body: Center(
-        child: Container(
-          width: 100,
-          height: 100,
-          color: Colors.limeAccent,
-          child: Text('Hello Devops!'),
-        ),
+          child: Text('Hello World', style: TextStyle(fontSize: 25))
       )
 
 
